22e9d7ed30ace0b49aaae85febb1190f9b3d787a
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-16 20:05:25 -0300

    feat: creating folders, components, services, interfaces

---
+++ src/app/interfaces/book.interface.ts
@@ -0,0 +1,11 @@
+import { Category } from "./category.interface";
+
+export interface Book {
+  id: number;
+  title: string;
+  author: string;
+  imageUrl?: string;
+  price: number;
+  categories: Category[];
+}
+
