ca3babe4532ba4f5377e30ad5f7dc247de953a9d
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-16 20:25:46 -0300

    fix: rename components and services

---
+++ src/app/services/book-service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Book } from '../interfaces/book.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookService {
+  
+  private http = inject(HttpClient);  
+
+  private readonly urlAPI = `${environment.API_URL}/book`; 
+
+  getBooks() : Observable<Book[]> {
+    return this.http.get<Book[]>(this.urlAPI);  
+  }
+
+}
