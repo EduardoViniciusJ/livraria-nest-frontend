1afc4c20100e3e1741a31b2c695087ad7f0ee891
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-16 22:11:58 -0300

    feat: create table book-component and navbar component

--- src/app/components/book-component/book-component.ts
+++ src/app/components/book-component/book-component.ts
@@ -3,10 +3,14 @@ import { Component, inject } from '@angular/core';
 import { ChipModule } from 'primeng/chip';
 import { TableModule } from 'primeng/table';
 import { BookService } from '../../services/book-service';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { TagModule } from 'primeng/tag';
+
 
 @Component({
   selector: 'app-book-component',
-  imports: [CommonModule, AsyncPipe, DecimalPipe, TableModule, ChipModule],
+  imports: [AsyncPipe, TableModule, ChipModule, ButtonModule, CardModule, TagModule, CommonModule],
   standalone: true, 
   templateUrl: './book-component.html',
   styleUrl: './book-component.css',
@@ -15,5 +19,6 @@ export class BookComponent {
 
   bookService = inject(BookService);  
   books$ = this.bookService.getBooks();
+ 
 }
   
