f8a5c0c2b6c05cb56d819aef5c138ae700d0e2e8
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-18 13:38:40 -0300

    feat: add table categories

--- src/app/components/category-component/category-component.ts
+++ src/app/components/category-component/category-component.ts
@@ -2,37 +2,116 @@ import { Component, inject } from '@angular/core';
 import { CategoryService } from '../../services/category-service';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
-import { ChipModule } from 'primeng/chip';
 import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
-import { TagModule } from 'primeng/tag';
-import { DataViewModule } from 'primeng/dataview';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { FormsModule } from '@angular/forms';
-import { ToolbarModule } from 'primeng/toolbar';
-import { FileUploadModule } from 'primeng/fileupload';
-import { RatingModule } from 'primeng/rating';
+import { DialogModule } from 'primeng/dialog';
+import { FormsModule, ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { Category } from '../../interfaces/category-interface';
+import { CategoryFormComponent } from '../category-form-category/category-form-category';
 
 @Component({
   selector: 'app-category-component',
-  imports: [AsyncPipe, TableModule, ChipModule, ButtonModule, CardModule, TagModule, CommonModule, DataViewModule, ButtonModule, SelectButtonModule, FormsModule, ToolbarModule, FileUploadModule, RatingModule],
   standalone: true,
+  providers: [MessageService],
+  imports: [
+    AsyncPipe, CommonModule, TableModule, ButtonModule, DialogModule,
+    FormsModule, ReactiveFormsModule, ToastModule,   
+  ],
   templateUrl: './category-component.html',
   styleUrl: './category-component.css',
 })
 export class CategoryComponent {
+
   categoryService = inject(CategoryService);
+
+  fb = inject(FormBuilder);
+
+  messageService = inject(MessageService);
 
   categories$ = this.categoryService.getCategory();
 
-  deleteCategory(category: any) {
-  this.categoryService.deleteCategory(category.id).subscribe({
-    next: () => {
-      this.categories$ = this.categoryService.getCategory(); 
-    },
-    error: (err) => {
-      console.error('Erro ao excluir categoria', err);
-    }
+  dialogVisible = false;
+  selectedCategory: Category | null = null;
+
+  form: FormGroup = this.fb.group({
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(254),
+        Validators.pattern(/^(?!\s+$).+/)
+      ]
+    ]
   });
-}
+
+  // Abre o form para criar ou editar uma categoria
+  openDialog(category: Category | null = null) {
+    this.selectedCategory = category;
+
+    if (category) {
+      this.form.patchValue(category);
+    } else {
+      this.form.reset();
+    }
+
+    this.dialogVisible = true;
+  }
+
+  // Fecha o form e reseta 
+  closeDialog() {
+    this.dialogVisible = false;
+    this.selectedCategory = null;
+    this.form.reset();
+  }
+
+  // Salva categoria 
+  saveCategory() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const formData = {
+      name: this.form.value.name.trim()
+    };
+
+    if (this.selectedCategory) {
+      this.categoryService.updateCategory(this.selectedCategory.id, formData).subscribe({
+        next: () => {
+          this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: 'Categoria atualizada com sucesso!' });
+          this.closeDialog();
+          this.categories$ = this.categoryService.getCategory();
+        },
+        error: (err) => {
+          this.messageService.add({ severity: 'error', summary: 'Erro', detail: err.error.message || 'Erro ao atualizar categoria.' });
+        }
+      });
+    } else {
+      this.categoryService.createCategory(formData).subscribe({
+        next: () => {
+          this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: 'Categoria criada com sucesso!' });
+          this.closeDialog();
+          this.categories$ = this.categoryService.getCategory();
+        },
+        error: (err) => {
+          this.messageService.add({ severity: 'error', summary: 'Erro', detail: err.error.message || 'Erro ao criar categoria.' });
+        }
+      });
+    }
+  }
+
+  // Exclui uma categoria
+  deleteCategory(category: Category) {
+    this.categoryService.deleteCategory(category.id).subscribe({
+      next: () => {
+        this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: 'Categoria excluída com sucesso!' });
+        this.categories$ = this.categoryService.getCategory();
+      },
+      error: (err) => {
+        this.messageService.add({ severity: 'error', summary: 'Erro ao excluir', detail: err.error.message });
+      }
+    });
+  }
 }
